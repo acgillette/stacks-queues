b8bcd1cf8cb8c296c8e823bc0b68c1c0f8cb02b7
Anacelia Gillette <acgillette426@gmail.com>
2017-02-23 12:23:41 -0800

    Finished Stacks and Queue implementation

--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,23 @@ class Stack
   end
 
   def push(element)
+    return @store.push(element)
   end
 
   def pop
+    return @store.pop
   end
 
   def top
+    return @store[-1]
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
   end
 
   def to_s
